c0ec651664ebfcea7c0d286bdaa385647ca669b9
Franlky01 <Franklynoblez@gmail.com>
2021-09-18 03:48:53 +0100

    Add  edited base layout , and handlers file

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -177,6 +177,8 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
+	//Remove session after being used
+	m.App.Session.Remove(r.Context(), "reservation")
 	data := make(map[string]interface{})
 	data["reservation"] = reservation
 	render.RenderTemplate(w, r, "reservation-summary.page.gohtml", &Models.TemplateData{
